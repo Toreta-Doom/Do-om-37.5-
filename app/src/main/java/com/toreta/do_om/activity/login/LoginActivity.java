32c228bb42c44a800de0268f856794d6dc2917bb
yeLyn36 <s2018w11@e-mirim.hs.kr>
2019-12-22 02:06:45 +0900

    sign_in

--- app/src/main/java/com/toreta/do_om/activity/login/LoginActivity.java
+++ app/src/main/java/com/toreta/do_om/activity/login/LoginActivity.java
@@ -1,19 +1,30 @@
 package com.toreta.do_om.activity.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.toreta.do_om.R;
+import com.toreta.do_om.activity.MainActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button sign_in_btn;
     private TextView register_Text;
+    private EditText sign_in_id;
+    private EditText sign_in_pw;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +33,36 @@ public class LoginActivity extends AppCompatActivity {
 
         init();
         buttonListener();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        sign_in_btn.setOnClickListener(new View.OnClickListener() {
+            String email = sign_in_id.getText().toString();
+            String pw = sign_in_pw.getText().toString();
+
+            @Override
+            public void onClick(View view) {
+                firebaseAuth.signInWithEmailAndPassword(email, pw).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(LoginActivity.this, "로그인 오류", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 
     private void init() {
 
         sign_in_btn = findViewById(R.id.sign_in_btn);
         register_Text = findViewById(R.id.register_Text);
+        sign_in_id = findViewById(R.id.sign_in_id);
+        sign_in_pw = findViewById(R.id.sign_in_pw);
 
     }
 
