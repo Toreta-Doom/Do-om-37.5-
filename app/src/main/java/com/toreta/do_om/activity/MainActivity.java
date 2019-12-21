5086de1be749b08914b0d75f1ab85b374ae3a358
B1ns <qlsl3574@naver.com>
2019-12-22 03:07:34 +0900

    add ReadFundingActivity

--- app/src/main/java/com/toreta/do_om/activity/MainActivity.java
+++ app/src/main/java/com/toreta/do_om/activity/MainActivity.java
@@ -8,9 +8,12 @@ import com.toreta.do_om.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
